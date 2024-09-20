7f350d86298a6abf8caed2cc6251d2a48db596f8
Daniel Maramba <daniel413x@gmail.com>
2024-09-20 17:04:07 -0400

    change staging database deployment logic

--- Databases/deploy-database.sh
+++ Databases/deploy-database.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # Check if the required number of arguments are provided
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 3 ]; then
     # e.g.
-    # deploy-database.sh user-test user postgres postgres
-    # service-name should be the same as defined in the yaml metadata
-    # metadata:
-    # name: user
-    echo "Usage: $0 <namespace> <service-name> <postgres-username> <postgres-password>"
+    # deploy-database.sh user-test postgres postgres
+    echo "Usage: $0 <namespace> <postgres-username> <postgres-password>"
     exit 1
 fi
 
 NAMESPACE=$1
-SERVICE_NAME=$2
-POSTGRES_USERNAME=$3
-POSTGRES_PASSWORD=$4
+POSTGRES_USERNAME=$2
+POSTGRES_PASSWORD=$3
 
 # check if namespace exists, create if it doesn't
 if ! kubectl get namespace "$NAMESPACE" >/dev/null 2>&1; then
@@ -25,13 +21,9 @@ else
     echo "Namespace $NAMESPACE already exists."
 fi
 
-# replace placeholders (<service-name>) in yamls with the passed arguments
+# replace placeholders (<postgres-user>, <postgres-password>) in yamls with the passed arguments
 sed -i "s/<postgres-user>/$POSTGRES_USERNAME/" postgres-secret.yaml
 sed -i "s/<postgres-password>/$POSTGRES_PASSWORD/" postgres-secret.yaml
-sed -i "s/<service-name>/$SERVICE_NAME/" postgres-secret.yaml
-sed -i "s/<service-name>/$SERVICE_NAME/" postgres-deployment.yaml
-sed -i "s/<service-name>/$SERVICE_NAME/" postgres-service.yaml
-sed -i "s/<service-name>/$SERVICE_NAME/" initdb-configmap.yaml
 
 # apply kubectl commands using the arg namespace
 kubectl delete -f initdb-configmap.yaml --namespace=$NAMESPACE
