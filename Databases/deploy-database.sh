5f083f51d54c1c74763acf52c8b9d7aa777cea81
Daniel Maramba <daniel413x@gmail.com>
2024-09-19 23:52:30 -0400

    add placeholders

--- Databases/deploy-database.sh
+++ Databases/deploy-database.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
+# Check if the required number of arguments are provided
+if [ "$#" -ne 4 ]; then
+    # e.g.
+    # deploy-database.sh user-test user postgres postgres
+    # service-name should be the same as defined in the yaml metadata
+    # metadata:
+    # name: user
+    echo "Usage: $0 <namespace> <service-name> <postgres-username> <postgres-password>"
+    exit 1
+fi
+
+NAMESPACE=$1
+SERVICE_NAME=$2
+POSTGRES_USERNAME=$3
+POSTGRES_PASSWORD=$4
+
 cd kubernetes
-sed -i "s/<postgres-user>/$DATABASE_USERNAME/" postgres-secret.yaml
-sed -i "s/<postgres-password>/$DATABASE_PASSWORD/" postgres-secret.yaml
-kubectl delete -f initdb-configmap.yaml --namespace=$1
-kubectl delete -f postgres-deployment.yaml --namespace=$1
-kubectl delete -f postgres-service.yaml --namespace=$1
-kubectl delete -f postgres-secret.yaml --namespace=$1
-kubectl apply -f initdb-configmap.yaml --namespace=$1
-kubectl apply -f postgres-secret.yaml --namespace=$1
-kubectl apply -f postgres-service.yaml --namespace=$1
-kubectl apply -f postgres-deployment.yaml --namespace=$1
-kubectl describe pods --namespace=$1
+
+# replace placeholders (<service-name>) in yamls with the passed arguments
+sed -i "s/<postgres-user>/$POSTGRES_USERNAME/" postgres-secret.yaml
+sed -i "s/<postgres-password>/$POSTGRES_PASSWORD/" postgres-secret.yaml
+sed -i "s/<service-name>/$SERVICE_NAME/" postgres-secret.yaml
+sed -i "s/<service-name>/$SERVICE_NAME/" postgres-deployment.yaml
+sed -i "s/<service-name>/$SERVICE_NAME/" postgres-service.yaml
+sed -i "s/<service-name>/$SERVICE_NAME/" initdb-configmap.yaml
+
+# apply kubectl commands using the arg namespace
+kubectl delete -f initdb-configmap.yaml --namespace=$NAMESPACE
+kubectl delete -f postgres-deployment.yaml --namespace=$NAMESPACE
+kubectl delete -f postgres-service.yaml --namespace=$NAMESPACE
+kubectl delete -f postgres-secret.yaml --namespace=$NAMESPACE
+
+kubectl apply -f initdb-configmap.yaml --namespace=$NAMESPACE
+kubectl apply -f postgres-secret.yaml --namespace=$NAMESPACE
+kubectl apply -f postgres-service.yaml --namespace=$NAMESPACE
+kubectl apply -f postgres-deployment.yaml --namespace=$NAMESPACE
+
+kubectl describe pods --namespace=$NAMESPACE
