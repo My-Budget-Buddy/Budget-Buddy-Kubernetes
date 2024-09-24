1ae2eba807e618b1539611baad6dda08c5108ae8
Aaron Huggins <achuggi1@asu.edu>
2024-09-23 17:52:41 -0700

    Create BuildFrontend.sh
    Created a script to build the frontend, a common thing in the Frontend Jenkinsfile

---
+++ Scripts/BuildFrontend.sh
@@ -0,0 +1,11 @@
+# Ensure argument count
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <api-endpoint>"
+    exit 1
+fi
+
+API_ENDPOINT=$1
+
+rm .env
+echo "VITE_BASE_API_ENDPOINT=$API_ENDPOINT" > .env
+npm install && npm run build
