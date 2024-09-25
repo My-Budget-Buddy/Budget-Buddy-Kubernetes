f360ec10975632294e91108b64ff2f5bf790598c
Daniel Maramba <daniel413x@gmail.com>
2024-09-25 14:58:17 -0400

    delete database initially in deploy-database.sh

--- Databases/deploy-database.sh
+++ Databases/deploy-database.sh
@@ -23,37 +23,18 @@ fi
 
 # MUTATES FILES!
 # replace placeholders (<postgres-user>, <postgres-password>) in yamls with the passed arguments
-# sed -i "s/<postgres-user>/$POSTGRES_USERNAME/" postgres-secret.yaml
-# sed -i "s/<postgres-password>/$POSTGRES_PASSWORD/" postgres-secret.yaml
+sed -i "s/<postgres-user>/$POSTGRES_USERNAME/" postgres-secret.yaml
+sed -i "s/<postgres-password>/$POSTGRES_PASSWORD/" postgres-secret.yaml
 
 # apply initdb-configmap.yaml only if it doesn't exist
-if ! kubectl get configmap initdb-configmap --namespace=$NAMESPACE >/dev/null 2>&1; then
-    echo "Applying initdb-configmap.yaml..."
-    kubectl apply -f initdb-configmap.yaml --namespace=$NAMESPACE
-else
-    echo "ConfigMap initdb-configmap already exists."
-fi
+kubectl delete -f initdb-configmap.yaml --namespace=$NAMESPACE || true
+kubectl apply -f initdb-configmap.yaml --namespace=$NAMESPACE
 
-# apply postgres-secret.yaml only if the secret doesn't exist
-if ! kubectl get secret postgres-secret --namespace=$NAMESPACE >/dev/null 2>&1; then
-    echo "Applying postgres-secret.yaml..."
-    kubectl apply -f postgres-secret.yaml --namespace=$NAMESPACE
-else
-    echo "Secret postgres-secret already exists."
-fi
+kubectl delete -f postgres-secret.yaml --namespace=$NAMESPACE || true
+kubectl apply -f postgres-secret.yaml --namespace=$NAMESPACE
 
-# apply postgres-service.yaml only if the service doesn't exist
-if ! kubectl get service postgres --namespace=$NAMESPACE >/dev/null 2>&1; then
-    echo "Applying postgres-service.yaml..."
-    kubectl apply -f postgres-service.yaml --namespace=$NAMESPACE
-else
-    echo "Service postgres-service already exists."
-fi
+kubectl delete -f postgres-service.yaml --namespace=${NAMESPACE} || true
+kubectl apply -f postgres-service.yaml --namespace=$NAMESPACE
 
-# apply postgres-deployment.yaml only if the deployment doesn't exist
-if ! kubectl get deployment postgres --namespace=$NAMESPACE >/dev/null 2>&1; then
-    echo "Applying postgres-deployment.yaml..."
-    kubectl apply -f postgres-deployment.yaml --namespace=$NAMESPACE
-else
-    echo "Deployment postgres-deployment already exists."
-fi
+kubectl delete -f postgres-deployment.yaml --namespace=$NAMESPACE || true
+kubectl apply -f postgres-deployment.yaml --namespace=$NAMESPACE
